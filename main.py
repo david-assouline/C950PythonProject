747d09a7cdf2a9efd650885b24eda227becf0c5c
David <david.assouline@hotmail.com>
2022-11-07 22:43:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import csv
+with open('data/distance_data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
